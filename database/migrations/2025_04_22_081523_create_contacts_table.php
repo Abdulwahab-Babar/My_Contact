49bde35299b8856a1575c984dd2873fce031a68e
Abdulwahab-Babar <abdulwahabbabar45@gmail.com>
2025-04-22 15:14:03 +0500

    updated the up() method in contact table

--- database/migrations/2025_04_22_081523_create_contacts_table.php
+++ database/migrations/2025_04_22_081523_create_contacts_table.php
@@ -11,9 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contacts', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table){
             $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
